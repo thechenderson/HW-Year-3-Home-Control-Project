9581cf6f92297d47543aa3e364e97e4dc05fad62
ml85 <ml85@jove>
2020-02-01 11:43:48 +0000

    added connection to database doesn't work inside gitHub forked folders

--- HW-Year-3-Home-Control-Project/src/Clock.java
+++ HW-Year-3-Home-Control-Project/src/Clock.java
@@ -1,32 +1,14 @@
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.Scanner;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.KeyStroke;
-import javax.swing.SwingConstants;
-import javax.swing.border.EmptyBorder;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Clock {
-	
+
 	//Clock variables
 //	private JFrame frame;
 //	private JLabel label;
@@ -38,15 +20,21 @@ public class Clock {
 	private static Clock c;
 	private static int startTimeH;
 	private static int startTimeM;
-	
+
 	//Device
-	private static Device d1, d2, d3, d4, d5;	
+	private static Device d1, d2, d3, d4, d5;
+	
+	//DBConnection
+//	private DBConnection db;
+	private static Connection conn;
+	
+	private String previousDevice = null;
 
 	public Clock(){
 		clock = new ClockDisplay();
 	}
 
-	
+
 	public void start(int startTimeH, int startTimeM)
 	{
 		clockRunning = true;
@@ -54,27 +42,27 @@ public class Clock {
 		timerThread.start();
 		clock.setTime(startTimeH, startTimeM);
 	}
-
+	
 
 	class TimerThread extends Thread
 	{
 		public void run()
 		{
 			String currentTime, deviceStart, deviceEnd = null;
-			
+
 			//Temporally hard coding devices in
 			d1 = new Device("Heating", 300,"06:30", "08:30");
 			d2 = new Device("Bedroom Light", 500, "07:00", "11:00"); //could be turned off by application
 			d3 = new Device("Kitchen Light", 500, "07:10", "07:30");
 			d4 = new Device("Bathroom Light", 300,"07:30", "07:40");
 			d5 = new Device("Plug", 200, "10:00", "11:00");
-			
+
 			Device[] devices = {d1, d2, d3, d4, d5};
 			ArrayList<Device> runningDevices = new ArrayList<Device>();
-			
+
 			while (clockRunning) {
 				currentTime = clock.getTime();
-				
+
 				for (Device d: devices) {
 
 					currentTime = clock.getTime();
@@ -84,31 +72,85 @@ public class Clock {
 
 					if ((deviceStart).equals(currentTime)) {
 						runningDevices.add(d);
-						System.out.println(currentTime + " " + d.getDeviceName());
+						try {
+							sendingData(d, currentTime);
+							
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						//System.out.println(currentTime + " " + d.getDeviceName());
 						clock.timeTick();
 					}
-					
+
 				}
-				
+
 				System.out.print(currentTime + " ");
-				
+
 				for(Device d: runningDevices) {
 					System.out.print(d.getDeviceName() + " ");
 					deviceEnd = d.getTimeOff();
-					
+//					try {
+//						//sendingData(d, currentTime);
+//					} catch (SQLException e) {
+//						// TODO Auto-generated catch block
+//						e.printStackTrace();
+//					}
+
 					if ((deviceEnd).equals(currentTime)) {
 						runningDevices.remove(d);
+						try {
+							deletingData(d, currentTime);
+						} catch (SQLException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 						break;
 					}
 				}
-				
+
 				clock.timeTick();
 				System.out.println();
 				pause();
+
+			}
+		}
+		
+		private void deletingData(Device d, String currentTime) throws SQLException {
+			setup();
+			Statement stmt = conn.createStatement();
+			String condition = "WHERE Name = '" + d.getDeviceName() + "'";
+			stmt.executeUpdate("DELETE FROM CurrentDevices " + condition);
+			previousDevice = d.getDeviceName();
+			
+		}
+
+		public void setup() throws SQLException {
+			try {
+				conn = DriverManager.getConnection("jdbc:mysql://mysql-server-1.macs.hw.ac.uk/ml85","ml85","7FgNF2Z3KJ");
+
+				//			Statement stmt = conn.createStatement( );
+				//			stmt.executeUpdate("INSERT INTO CurrentDevices " + "VALUES ('leo', 100, '09:00', '09:10')");		
+
+			} catch (Exception e) {
+				System.out.println("Error - no connection to database");
+			} finally {
 				
 			}
 		}
 		
+		private void sendingData(Device d, String currentTime) throws SQLException {
+			//could vary the power number for graph
+			if (previousDevice != d.getDeviceName()) {
+				setup();
+				Statement stmt = conn.createStatement();
+				String values = "VALUES ('" + d.getDeviceName() + "'," + d.getPower() + ",'" + d.getTimeOn() + "', '" + d.getTimeOff() + "')";
+				stmt.executeUpdate("INSERT INTO CurrentDevices " + values);
+				previousDevice = d.getDeviceName();
+			}
+			
+		}
+
 		private void pause()
 		{
 			try {
@@ -120,10 +162,10 @@ public class Clock {
 	}
 
 
-	public static void main(String[] args) throws ParseException
-	{	
+	public static void main(String[] args) throws ParseException, SQLException
+	{
 		Scanner scan = new Scanner(System.in);
-
+		
 		//Ask for start time
 		System.out.println("What time would you like to start the simulation at? (24-Hour) ");
 		startTimeH = scan.nextInt();
@@ -135,8 +177,9 @@ public class Clock {
 		c = new Clock();
 		c.start(startTimeH, startTimeM);
 		
+		scan.close();
+		conn.close();
+
 	}
 
 }
-
-

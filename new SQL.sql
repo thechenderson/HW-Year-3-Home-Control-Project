9a0a6f8f15005dfd2f711b628159b1d9de9ceb80
Thomas Gray <56586500+ThomasGray3@users.noreply.github.com>
2020-02-04 23:03:15 +0000

    with join table

---
+++ new SQL.sql
@@ -0,0 +1,38 @@
+CREATE TABLE `rooms` (
+  `roomID` int(3),
+  `roomDisplayName` varchar(20) NOT NULL,
+  `roomType` varchar(20) NOT NULL,
+  PRIMARY KEY (roomID)
+) ENGINE=INNODB;
+
+
+
+CREATE TABLE `devices` (
+  `deviceID` int(11) NOT NULL,
+  `deviceDisplayName` varchar(20) NOT NULL,
+  `devicePower` int(11) NOT NULL,
+  `deviceType` int(11) NOT NULL,
+  `roomID` int(3) NOT NULL,
+  PRIMARY KEY (deviceID),
+  FOREIGN KEY (roomID) REFERENCES rooms(roomID)
+) ENGINE=INNODB;
+
+
+
+
+CREATE TABLE `users` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL,
+  `displayName` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (username)
+) ENGINE=INNODB;
+
+
+CREATE TABLE `homes` (
+  `username` varchar(20),
+  `roomID` int(3),
+  PRIMARY KEY (username, roomID),
+  FOREIGN KEY (username) REFERENCES users(username),
+  FOREIGN KEY (roomID) REFERENCES rooms(roomID)
+) ENGINE=INNODB;

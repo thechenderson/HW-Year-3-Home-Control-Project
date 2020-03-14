ec56301ea153d7e17eea996a678f0dade04808e0
chenderson99 <chris.henderson1999@gmail.com>
2020-03-14 21:30:46 +0000

    Potentially fucked commit. May need rolled back if doesn't work on other devices. Fixed broken manage users page

--- EcoSpark-App/files/routes/manage-users.js
+++ EcoSpark-App/files/routes/manage-users.js
@@ -1,29 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-var connection = mysql.createConnection({
-  host: process.env.hostname,
-  user: process.env.username,
-  password: process.env.password,
-  database: process.env.database
-});
-
-
 router.get('/', function(req, res, next) {
     if (req.session.loggedin){
-      res.render('manage-users', { title: 'Express' });
+      res.render('manage-rooms', { title: 'Express' });
     } else {
       res.redirect('/');
     }
   });
-
-
-
-  connection.connect(function(err) {
-    if (err) throw err;
-    con.query("SELECT * FROM customers", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-    });
-
 module.exports = router;

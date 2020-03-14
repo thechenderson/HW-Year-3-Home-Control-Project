bd3b01ddda5277efa7e20839563af61c922a69b1
chenderson99 <chris.henderson1999@gmail.com>
2020-03-14 21:14:03 +0000

    Added basic manage users page, tidied rooms and header UI.

--- EcoSpark-App/files/routes/manage-users.js
+++ EcoSpark-App/files/routes/manage-users.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var connection = mysql.createConnection({
+  host: process.env.hostname,
+  user: process.env.username,
+  password: process.env.password,
+  database: process.env.database
+});
+
+
 router.get('/', function(req, res, next) {
     if (req.session.loggedin){
       res.render('manage-users', { title: 'Express' });
@@ -10,5 +18,12 @@ router.get('/', function(req, res, next) {
   });
 
 
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT * FROM customers", function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    });
 
 module.exports = router;

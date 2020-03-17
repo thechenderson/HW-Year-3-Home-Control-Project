4bc8b3bb725d102ff48014f2f43db26c2e6d42d0
tg37 <tg37@hw.ac.uk>
2020-03-17 22:12:43 +0000

    updates

--- graphExample/data.php
+++ graphExample/data.php
@@ -5,8 +5,8 @@ header('Content-Type: application/json');
 //database
 define('DB_HOST', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'password');
-define('DB_NAME', 'ecospark');
+define('DB_PASSWORD', 'countlich1');
+define('DB_NAME', 'ecosparknew');
 
 //get connection
 $mysqli = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);

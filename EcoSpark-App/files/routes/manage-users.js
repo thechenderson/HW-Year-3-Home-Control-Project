34b6b4bdaa21c596ebc319ea722c0f3982fa0211
chenderson99 <chris.henderson1999@gmail.com>
2020-03-18 10:45:41 +0000

    Fucked up the users page but added sql for it

--- EcoSpark-App/files/routes/manage-users.js
+++ EcoSpark-App/files/routes/manage-users.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: process.env.hostname,
+  user: process.env.username,
+  password: process.env.password,
+  database: process.env.database
+});
+
 
 router.get('/', function(req, res, next) {
     if (req.session.loggedin){
-      res.render('manage-users', { title: 'Express' });
+      var sql = "SELECT users.displayName AS displayName, users.username AS username FROM users, homes WHERE users.homeID = homes.homeID";
+      connection.query(sql, function(err, result, fields) {
+      res.render('manage-users', ({ title: 'Express' }, {users: result, home: req.session.homeID}));
+      });
     } else {
       res.redirect('/');
     }

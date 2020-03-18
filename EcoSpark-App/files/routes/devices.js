002c0d8ef6925d771643d2729488b8f28f560d29
Meabh Lowry <ml85@hw.ac.uk>
2020-03-18 14:40:20 +0000

    merge

--- EcoSpark-App/files/routes/devices.js
+++ EcoSpark-App/files/routes/devices.js
@@ -22,7 +22,49 @@ const queryWrapper = (statement) => {
 
 router.get('/', function(req, res, next) {
   if (req.session.loggedin){
+    // var $ = require('jQuery');
+    var jsdom = require('jsdom');
+    $ = require('jquery')(new jsdom.JSDOM().window); 
+
     
+    $.ajax({
+      //might be the problem here as it linking to my other server file, the data.php file is in the php folder
+      url: "http://localhost/SE/Ex%205%20-%20DevicesGraph2/data.php",
+      method: "GET",
+      success: function(data) {
+      console.log(data);
+      var device = [];
+      var power = [];
+      for(var i in data) {
+        device.push("Device " + data[i].rDeviceDisplayName);
+        power.push(data[i].rDevicePower);
+      }
+      var chartdata = {
+        labels: device,
+        datasets : [
+        {
+          label: 'DevicePower',
+          backgroundColor: 'rgba(200, 200, 200, 0.75)',
+          borderColor: 'rgba(200, 200, 200, 0.75)',
+          hoverBackgroundColor: 'rgba(200, 200, 200, 1)',
+          hoverBorderColor: 'rgba(200, 200, 200, 1)',
+          data: power
+        }
+      ]
+    };
+    var ctx = $("#mycanvas");
+
+    var barGraph = new Chart(ctx, {
+      type: 'bar',
+      data: chartdata
+    });
+    },
+    error: function(data) {
+    console.log(data);
+    }
+    });
+  
+
     var sqlR = "SELECT rooms.roomDisplayName AS roomDisplayName, rooms.roomID AS roomID FROM users, rooms, homes WHERE users.homeID = homes.homeID AND homes.homeID = rooms.homeID AND users.username = '" + req.session.user + "'";
     var sqlD = "SELECT devices.deviceDisplayName AS deviceDisplayName, devices.deviceType AS deviceType, devices.deviceID AS deviceID, devices.devicePower AS devicePower, devices.roomID AS roomID FROM devices, rooms, users, homes WHERE users.homeID = homes.homeID AND homes.homeID = rooms.homeID AND rooms.roomID = devices.roomID AND users.username = '" + req.session.user + "'";
     var sqlF = "SELECT faults.deviceID AS fDeviceID, faults.deviceDisplayName AS fDeviceDisplayName, faults.roomDisplayName AS fRoomDisplayName, faults.faultInfo AS faultInfo FROM faults";

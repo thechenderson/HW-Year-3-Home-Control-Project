c8d82fb07241c7dbf72f75a48748fc8fe02e9108
Meabh Lowry <ml85@hw.ac.uk>
2020-04-04 17:22:01 +0100

    site settings - font size

---
+++ EcoSpark-App/files/routes/font.js
@@ -0,0 +1,20 @@
+
+$('.change-font').on('click', 'h1', function() {
+console.log('clcik');
+$("body").removeClass();
+$("body").addClass("font-large");
+});
+    
+$('.change-font').on('click', 'h2', function() {
+$("body").removeClass();
+$("body").addClass("font-medium");
+});
+    
+$('.change-font').on('click', 'h3', function() {
+$("body").removeClass();
+$("body").addClass("font-small");
+});
+    
+$('.change-font').on('click', 'button', function() {
+$("body").removeClass();
+});

0eb021994eba7c3f784044460e2bc4e5ee770732
tg37 <tg37@hw.ac.uk>
2020-04-13 13:04:01 +0100

    manage users done

--- EcoSpark-App/files/routes/settings.js
+++ EcoSpark-App/files/routes/settings.js
@@ -4,11 +4,11 @@ var mysql = require('mysql');
 
 const queryWrapper = (statement) => {
   return new Promise((resolve, reject) => {
-      connection.query(statement, (err, result) => {
-          if(err)
-              return reject(err);
-          resolve(result);
-      });
+    connection.query(statement, (err, result) => {
+      if (err)
+        return reject(err);
+      resolve(result);
+    });
   });
 };
 
@@ -19,26 +19,26 @@ var connection = mysql.createConnection({
   database: process.env.database
 });
 
-router.get('/', function(req, res, next) {
-  if (req.session.loggedin){
+router.get('/', function (req, res, next) {
+  if (req.session.loggedin) {
     res.render('settings', { title: 'Express' });
   } else {
     res.redirect('/');
   }
 });
 
-router.get('/manage-users', function(req, res, next) {
-  if (req.session.loggedin){
+router.get('/manage-users', function (req, res, next) {
+  if (req.session.loggedin) {
     // sql to get the users homeID
-    var sql = "SELECT homeID FROM users WHERE users.username = '" + req.session.user +"'";
-    connection.query(sql, function(err, result, fields) {
+    var sql = "SELECT homeID FROM users WHERE users.username = '" + req.session.user + "'";
+    connection.query(sql, function (err, result, fields) {
       // index result to get the homeID without sql jargon
       var homeID = result[0].homeID;
       // query using homeID
       var sql2 = "SELECT username, displayName FROM users WHERE homeID = '" + homeID + "'";
-      connection.query(sql2, function(err, result2, fields) {
+      connection.query(sql2, function (err, result2, fields) {
         // render page 
-        res.render('manage-users', ({ title: 'Express' },{users: result2}));
+        res.render('manage-users', ({ title: 'Express' }, { users: result2 }));
       });
     });
   } else {
@@ -47,63 +47,63 @@ router.get('/manage-users', function(req, res, next) {
 });
 
 // when clicking on a user
-router.get('/manage-users/:username', function(req, res, next) {
-  if (req.session.loggedin){
+router.get('/manage-users/:username', function (req, res, next) {
+  if (req.session.loggedin) {
     var sqlD = "SELECT users.username AS username, users.displayName AS displayName, users.isAdmin AS isAdmin, users.password AS password, users.homeID AS homeID, homes.homeName AS homeName FROM users, homes WHERE users.homeID = homes.homeID AND users.username ='" + req.params.username + "'";
     Promise.all([
-        queryWrapper(sqlD)
+      queryWrapper(sqlD)
     ])
-    .then(([userInfo]) => {
-      res.render('specific-user', {
-        title: 'Express',
-        userInfo
+      .then(([userInfo]) => {
+        res.render('specific-user', {
+          title: 'Express',
+          userInfo
+        });
       });
-    });
   } else {
     res.redirect('/');
   }
 });
 
-
-router.post('/manage-users/:username/updateUserName', function(req, response) {
+//update name
+router.post('/manage-users/:username/updateUserName', function (req, response) {
   var username = req.params.username;
   var displayNameOld = req.body.username;
-  var displayName = displayNameOld.replace(/[^a-zA-Z0-9]/g,"");
+  var displayName = displayNameOld.replace(/[^a-zA-Z0-9]/g, "");
   var sql = "UPDATE users SET displayName = '" + displayName + "' WHERE users.username = '" + username + "'";
   connection.query(sql, function (err, result, fields) {
-    if(result== ""){
+    if (result == "") {
       response.redirect('/home/settings/manage-users');
     } else {
       console.log(result);
-      response.redirect('/home/settings/manage-users/' + req.params.username );
+      response.redirect('/home/settings/manage-users/' + req.params.username);
     }
   });
 });
 
-
-router.post('/manage-users/:username/updateUserPW', function(req, response) {
+// update password
+router.post('/manage-users/:username/updateUserPW', function (req, response) {
   var username = req.params.username;
   var PWOld = req.body.password;
-  var passwordNew = PWOld.replace(/[^a-zA-Z0-9]/g,"");
+  var passwordNew = PWOld.replace(/[^a-zA-Z0-9]/g, "");
   var sql = "UPDATE users SET password = '" + passwordNew + "' WHERE users.username = '" + username + "'";
   connection.query(sql, function (err, result, fields) {
-    if(result== ""){
+    if (result == "") {
       response.redirect('/home/settings/manage-users');
     } else {
       console.log(result);
-      response.redirect('/home/settings/manage-users/' + req.params.username );
+      response.redirect('/home/settings/manage-users/' + req.params.username);
     }
   });
 });
 
 
-
-router.post('/manage-users/:username/deleteUser', function(req, response) {
+// delete user
+router.post('/manage-users/:username/deleteUser', function (req, response) {
   var username = req.params.username;
   var sql = "DELETE FROM users WHERE users.username = '" + username + "'";
   connection.query(sql, function (err, result, fields) {
-    if(result== ""){
-      response.redirect('/home/settings/manage-users');
+    if (result == "") {
+      response.redirect('/home/settings/manage-users/' + req.params.username);
     } else {
       console.log(result);
       response.redirect('/home/settings/manage-users/');
@@ -112,16 +112,65 @@ router.post('/manage-users/:username/deleteUser', function(req, response) {
 });
 
 
+// checks to see if its not the last admin in the home 
+router.post('/manage-users/:username/removeFromHome', function (req, response) {
+  var sql1 = "SELECT homeID FROM users WHERE username = '" + req.params.username + "'";
+  connection.query(sql1, function (err, result1, fields) {
+    console.log(result1[0]);
+    var sql2 = "SELECT COUNT(username) AS number FROM users WHERE homeID ='" + result1[0].homeID + "' AND isAdmin = 'Yes'";
+    connection.query(sql2, function (err, result2, fields) {
+      console.log(result2[0]);
+      console.log(result2[0].number == 1);
+      if (result2[0].number == 1) {
+        response.redirect('/home/settings/manage-users/' + req.params.username);
+      } else {
+        var sql3 = "UPDATE users SET homeID = NULL WHERE username ='" + req.params.username + "'";
+        connection.query(sql3, function (err, result3, fields) {
+          if (result3 == "") {
+            response.redirect('/home/settings/manage-users/' + req.params.username);
+          } else {
+            var sql4 = "UPDATE users SET isAdmin = 'No' WHERE username ='" + req.params.username + "'";
+            connection.query(sql4, function (err, result4, fields) {
+              if (result4 == "") {
+                response.redirect('/home/settings/manage-users/' + req.params.username);
+              } else {
+                console.log(result4);
+                response.redirect('/home');
+              }
+            });
+          }
+        });
+      }
+    });
+  });
+});
 
-router.post('/manage-users/:username/removeFromHome', function(req, response) {
-  var username = req.params.username;
-  var sql = "UPDATE users SET homeID = NULL WHERE username ='" + username + "'";
-  connection.query(sql, function (err, result, fields) {
-    if(result== ""){
-      response.redirect('/home/settings/manage-users');
-    } else {
-      console.log(result);
-      response.redirect('/home/settings/manage-users');
+
+// checks if admin, and updates to the oposite
+router.post('/manage-users/:username/updateUserAdmin', function (req, response) {
+  var sqlCheck = "SELECT isAdmin FROM users WHERE username = '" + req.params.username + "'";
+  connection.query(sqlCheck, function (err, result, fields) {
+    if (result[0].isAdmin == "Yes") {
+      var sql = "UPDATE users SET isAdmin = 'No' WHERE username = '" + req.params.username + "'";
+      connection.query(sql, function (err, result, fields) {
+        if (result == "") {
+          response.redirect('/home/settings/manage-users/');
+        } else {
+          console.log(result);
+          response.redirect('/home/settings/manage-users/' + req.params.username);
+        }
+      });
+    }
+    if (result[0].isAdmin == "No") {
+      var sql = "UPDATE users SET isAdmin = 'Yes' WHERE username = '" + req.params.username + "'";
+      connection.query(sql, function (err, result, fields) {
+        if (result == "") {
+          response.redirect('/home/settings/manage-users/');
+        } else {
+          console.log(result);
+          response.redirect('/home/settings/manage-users/' + req.params.username);
+        }
+      });
     }
   });
 });
@@ -129,11 +178,7 @@ router.post('/manage-users/:username/removeFromHome', function(req, response) {
 
 
 
-
-
-
-
-router.post('/manage-home/:homeID/updateHome', function(req, response) {
+router.post('/manage-home/:homeID/updateHome', function (req, response) {
   var homeID = req.params.homeID;
   console.log(homeID);
   var homeName = req.body.homeName;
@@ -142,9 +187,9 @@ router.post('/manage-home/:homeID/updateHome', function(req, response) {
   // console.log(homeName);
   console.log(homeCreator);
 
-  var sql = "UPDATE homes SET homes.homeName = '" + homeName + "', homes.homeCreator = '"+ homeCreator +"' WHERE homes.homeID = '" + homeID + "'";
+  var sql = "UPDATE homes SET homes.homeName = '" + homeName + "', homes.homeCreator = '" + homeCreator + "' WHERE homes.homeID = '" + homeID + "'";
   connection.query(sql, function (err, result, fields) {
-    if(result== ""){
+    if (result == "") {
       response.redirect('/home/settings/manage-home');
     } else {
       response.redirect('/home/settings/manage-home/');
@@ -152,18 +197,18 @@ router.post('/manage-home/:homeID/updateHome', function(req, response) {
   });
 });
 
-router.get('/manage-devices', function(req, res, next) {
-  if (req.session.loggedin){
+router.get('/manage-devices', function (req, res, next) {
+  if (req.session.loggedin) {
     // sql to get the users homeID
-    var sql = "SELECT homeID FROM users WHERE users.username = '" + req.session.user +"'";
-    connection.query(sql, function(err, result, fields) {
+    var sql = "SELECT homeID FROM users WHERE users.username = '" + req.session.user + "'";
+    connection.query(sql, function (err, result, fields) {
       // index result to get the homeID without sql jargon
       var homeID = result[0].homeID;
       // query using homeID
-      var sql2 = "SELECT devices.deviceDisplayName AS deviceDisplayName, devices.deviceType AS deviceType, devices.deviceID AS deviceID, devices.devicePower AS devicePower, devices.roomID AS roomID FROM devices, rooms, homes WHERE homes.homeID = rooms.homeID AND rooms.roomID = devices.roomID AND homes.homeID = '"+homeID+"'";
-      connection.query(sql2, function(err, result2, fields) {
+      var sql2 = "SELECT devices.deviceDisplayName AS deviceDisplayName, devices.deviceType AS deviceType, devices.deviceID AS deviceID, devices.devicePower AS devicePower, devices.roomID AS roomID FROM devices, rooms, homes WHERE homes.homeID = rooms.homeID AND rooms.roomID = devices.roomID AND homes.homeID = '" + homeID + "'";
+      connection.query(sql2, function (err, result2, fields) {
         // render page 
-        res.render('manage-devices', ({ title: 'Express' },{devices: result2}));
+        res.render('manage-devices', ({ title: 'Express' }, { devices: result2 }));
       });
     });
   } else {
@@ -172,11 +217,11 @@ router.get('/manage-devices', function(req, res, next) {
 });
 
 // delete rooms and re-assign home?
-router.get('/manage-home', function(req, res, next) {
-  if (req.session.loggedin){
+router.get('/manage-home', function (req, res, next) {
+  if (req.session.loggedin) {
     var sql = "SELECT homes.homeID AS homeID, homes.homeName AS homeName, homes.homeCreator AS homeCreator FROM users, homes WHERE users.homeID = homes.homeID AND users.username = '" + req.session.user + "'";
-    connection.query(sql, function(err, result, fields) {
-      res.render('manage-home', ({ title: 'Express' }, {homes: result}));
+    connection.query(sql, function (err, result, fields) {
+      res.render('manage-home', ({ title: 'Express' }, { homes: result }));
     });
   } else {
     res.redirect('/');
@@ -184,32 +229,32 @@ router.get('/manage-home', function(req, res, next) {
 });
 
 // when updating a home
-router.get('/manage-home/:homeID/homeUpdate', function(req, res, next) {
-  var sql =  "UPDATE homes SET homeName = '" + request.params.homeName + "' WHERE homeID = '" + request.params.homeID + "'";
+router.get('/manage-home/:homeID/homeUpdate', function (req, res, next) {
+  var sql = "UPDATE homes SET homeName = '" + request.params.homeName + "' WHERE homeID = '" + request.params.homeID + "'";
   connection.query(sql, function (err, result, fields) {
-    if(result== ""){
+    if (result == "") {
       response.redirect('/home');
     } else {
-      response.redirect('/home/manage-home/' );
+      response.redirect('/home/manage-home/');
     }
   });
 });
 
-router.get('/help', function(req, res, next) {
-  if (req.session.loggedin){
-      res.render('help', ({ title: 'Express' }));
+router.get('/help', function (req, res, next) {
+  if (req.session.loggedin) {
+    res.render('help', ({ title: 'Express' }));
   } else {
     res.redirect('/');
   }
 });
 
 //account page
-router.get('/my-account', function(req, res, next) {
-  if (req.session.loggedin){
+router.get('/my-account', function (req, res, next) {
+  if (req.session.loggedin) {
     console.log(req.session.user);
     var sql = "SELECT users.username AS username, users.password AS password, users.isAdmin AS isAdmin, users.displayName AS displayName, users.homeID AS homeID, homes.homeName AS homeName FROM users, homes WHERE users.username ='" + req.session.user + "' AND users.homeID = homes.homeID;";
-    connection.query(sql, function(err, result, fields) {
-      res.render('my-account', ({ title: 'Express' }, {users: result}));
+    connection.query(sql, function (err, result, fields) {
+      res.render('my-account', ({ title: 'Express' }, { users: result }));
     });
   } else {
     res.redirect('/');

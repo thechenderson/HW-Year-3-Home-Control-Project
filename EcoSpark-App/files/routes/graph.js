d0aff00430d421e7f320978d5137a3adf501b6b1
tg37 <tg37@hw.ac.uk>
2020-04-16 11:47:31 +0100

    graph bug fix

--- EcoSpark-App/files/routes/graph.js
+++ EcoSpark-App/files/routes/graph.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
-var deviceNameGraph = [], devicePowerGraph = [];
-var jsonArray;
+
+
 
 var connection = mysql.createConnection({
     host: process.env.hostname,
@@ -12,26 +12,28 @@ var connection = mysql.createConnection({
 });
 
 
-function formatData(dataArray) {
-    console.log(dataArray);
+function formatDataRoom(dataArray) {
+    console.log(dataArray.length);
+    var deviceNameGraph = [], devicePowerGraph = [];
     for (var i = 0; i < dataArray.length; i++) {
         deviceNameGraph[i] = dataArray[i].rDeviceDisplayName;
         devicePowerGraph[i] = dataArray[i].rDevicePower;
     }
-    jsonArray = [deviceNameGraph, devicePowerGraph];
+    var jsonArray = [deviceNameGraph, devicePowerGraph];
     console.log("in FormatData()...\n");
     console.log(jsonArray);
+    return jsonArray
 }
 
 
-router.get('/', function (req, res) {
+router.get('/room', function (req, res) {
 
     var sqlRD = "SELECT rDeviceDisplayName, rDevicePower FROM runningdevices WHERE roomID = '" + req.session.room + "';";
 
     connection.query(sqlRD, function (err, data, fields) {
         console.log(data);
-        formatData(data);
-        res.send(jsonArray);
+        var formatedData = formatDataRoom(data);
+        res.send(formatedData);
     });
 });
 module.exports = router;
